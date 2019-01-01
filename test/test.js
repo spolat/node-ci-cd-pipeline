49a18b478600ca503b2a3314009c5a4098446b0a
spolat <suatppolat@gmail.com>
2019-01-01 19:44:36 +0300

    arrow function

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('Get', function () {
-    it('respond with hello world', function (done) {
+    it('respond with hello world', (done) => {
         return request(app).get('/').expect('hello world', done());
     });
 });
